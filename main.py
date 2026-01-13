5e7f2e39e7e70278b2f961d27c694f75fa21cbb4
Artem-Potapov <potapov2350@gmail.com>
2026-01-13 07:17:21 +0300

    [WIP] begin to ACTUALLY work on player

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import pygame
+import os
+import sys
+
+script_directory = os.path.dirname(os.path.realpath(__file__))
 
 pygame.init()
-pygame.display.set_mode((800, 600))
+display = pygame.display.set_mode((800, 600))
 
 FPS = 60
 running = True
 clock = pygame.time.Clock()
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load(f"player.png").convert()
+
+        self.rect = self.image.get_rect()
+
+        display.blit(self.image, (0,0 ))
+
+player = Player()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.update()
 
     clock.tick(FPS)
 
